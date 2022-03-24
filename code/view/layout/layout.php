44db6bc8c16387a2f126942a3b49df3056f0799d
Kevin Sunjaya <6181901008@student.unpar.ac.id>
2022-03-24 11:30:21 +0700

    header

--- code/view/layout/layout.php
+++ code/view/layout/layout.php
@@ -4,6 +4,7 @@
     <title>Topcoder FinTech</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="view/style/style.css">
+    <link rel="stylesheet" href="view/css/default_style.css">
     <!--BOXICON CDN link -->
     <link href='https://unpkg.com/boxicons@2.1.1/css/boxicons.min.css' rel='stylesheet'>
   </head>
@@ -37,6 +38,13 @@
         </ul>
       </section>
       <section>
+        <header>
+          <div>
+            <img src="src/thumbnail.png" alt="">
+            <p>[username]</p>
+            <a href="logout"><i class='bx bx-log-out bx-sm'></i>Logout</a>
+          </div>
+        </header>
         <?php echo $content ?>
       </section>
     </main>
