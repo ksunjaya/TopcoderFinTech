5de6cea30c0adb7c56fa0ec0a7690d69d3b9acfd
Kevin Sunjaya <6181901008@student.unpar.ac.id>
2022-03-24 10:39:36 +0700

    Update authLayout.php

--- code/view/layout/authLayout.php
+++ code/view/layout/authLayout.php
@@ -5,10 +5,6 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="view/style/login.css">
     <link rel="stylesheet" href="view/style/style.css">
-    <!-- <style>
-      <?php include "view/css/login_style.css" ?>
-      <?php include "view/css/default_style.css" ?>
-    </style> -->
   </head>
 
   <body>
